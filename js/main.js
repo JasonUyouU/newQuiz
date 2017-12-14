dc565944da6f7876a8b111b40e7b71f0923b2df3
Jason You <jasonyou92@gmail.com>
2017-12-14 12:29:16 -0800

    d.r.y changes using .each() function

--- js/main.js
+++ js/main.js
@@ -1,47 +1,18 @@
-$("#answers1 .answer").click(function(){
-  $("#correct1").css("background-color", "#ee8722");
-  $("#correct1 p").css("color", "black");
-  $("#correct1 span").css("color", "white");
-  setTimeout(function(){
-    $("#question1").addClass("hidden");
-    $("#answers1").addClass("hidden");
-    $("#question2").removeClass("hidden");
-    $("#answers2").removeClass("hidden");
-  }, 2000);
-})
-$("#answers2 .answer").click(function(){
-  $("#correct2").css("background-color", "#ee8722");
-  $("#correct2 p").css("color", "black");
-  $("#correct2 span").css("color", "white");
-  setTimeout(function(){
-    $("#question2").addClass("hidden");
-    $("#answers2").addClass("hidden");
-    $("#question3").removeClass("hidden");
-    $("#answers3").removeClass("hidden");
-  }, 2000);
-})
-$("#answers3 .answer").click(function(){
-  $("#correct3").css("background-color", "#ee8722");
-  $("#correct3 p").css("color", "black");
-  $("#correct3 span").css("color", "white");
-  setTimeout(function(){
-    $("#question3").addClass("hidden");
-    $("#answers3").addClass("hidden");
-    $("#question4").removeClass("hidden");
-    $("#answers4").removeClass("hidden");
-  }, 2000);
-})
-$("#answers4 .answer").click(function(){
-  $("#correct4").css("background-color", "#ee8722");
-  $("#correct4 p").css("color", "black");
-  $("#correct4 span").css("color", "white");
-  setTimeout(function(){
-    $("#question4").addClass("hidden");
-    $("#answers4").addClass("hidden");
-    $("#question5").removeClass("hidden");
-    $("#answers5").removeClass("hidden");
-  }, 2000);
-})
+//Iteration function of questions 1-4
+$.each([1, 2, 3, 4, 5], function(i) {
+  $("#answers" + i + " .answer").click(function() {
+    $("#correct" + i).css("background-color", "#ee8722");
+    $("#correct" + i + " p").css("color", "black");
+    $("#correct" + i + " span").css("color", "white");
+    setTimeout(function(){
+      $("#question" + i).addClass("hidden");
+      $("#answers" + i).addClass("hidden");
+      $("#question" + (i + 1)).removeClass("hidden");
+      $("#answers" + (i + 1)).removeClass("hidden");
+    }, 2000);
+  })
+});
+//click function for the last question
 $("#answers5 .answer").click(function(){
   $("#correct5").css("background-color", "#ee8722");
   $("#correct5 p").css("color", "black");
