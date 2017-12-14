94fed5fdf43deca4a33d3fd012baf67bdadce267
Jason You <jasonyou92@gmail.com>
2017-12-13 23:29:57 -0800

    changed colors and added set timeout

--- js/main.js
+++ js/main.js
@@ -1,24 +1,49 @@
-$("#correct1").click(function(){
-  $("#question1").addClass("hidden");
-  $("#answers1").addClass("hidden");
-  $("#question2").removeClass("hidden");
-  $("#answers2").removeClass("hidden");
+$("#answers1 .answer").click(function(){
+  $("#correct1").css("background-color", "#ee8722");
+  $("#correct1 p").css("color", "black");
+  $("#correct1 span").css("color", "white");
+  setTimeout(function(){
+    $("#question1").addClass("hidden");
+    $("#answers1").addClass("hidden");
+    $("#question2").removeClass("hidden");
+    $("#answers2").removeClass("hidden");
+  }, 2000);
 })
-$("#correct2").click(function(){
-  $("#question2").addClass("hidden");
-  $("#answers2").addClass("hidden");
-  $("#question3").removeClass("hidden");
-  $("#answers3").removeClass("hidden");
+$("#answers2 .answer").click(function(){
+  $("#correct2").css("background-color", "#ee8722");
+  $("#correct2 p").css("color", "black");
+  $("#correct2 span").css("color", "white");
+  setTimeout(function(){
+    $("#question2").addClass("hidden");
+    $("#answers2").addClass("hidden");
+    $("#question3").removeClass("hidden");
+    $("#answers3").removeClass("hidden");
+  }, 2000);
 })
-$("#correct3").click(function(){
-  $("#question3").addClass("hidden");
-  $("#answers3").addClass("hidden");
-  $("#question4").removeClass("hidden");
-  $("#answers4").removeClass("hidden");
+$("#answers3 .answer").click(function(){
+  $("#correct3").css("background-color", "#ee8722");
+  $("#correct3 p").css("color", "black");
+  $("#correct3 span").css("color", "white");
+  setTimeout(function(){
+    $("#question3").addClass("hidden");
+    $("#answers3").addClass("hidden");
+    $("#question4").removeClass("hidden");
+    $("#answers4").removeClass("hidden");
+  }, 2000);
 })
-$("#correct4").click(function(){
-  $("#question4").addClass("hidden");
-  $("#answers4").addClass("hidden");
-  $("#question5").removeClass("hidden");
-  $("#answers5").removeClass("hidden");
+$("#answers4 .answer").click(function(){
+  $("#correct4").css("background-color", "#ee8722");
+  $("#correct4 p").css("color", "black");
+  $("#correct4 span").css("color", "white");
+  setTimeout(function(){
+    $("#question4").addClass("hidden");
+    $("#answers4").addClass("hidden");
+    $("#question5").removeClass("hidden");
+    $("#answers5").removeClass("hidden");
+  }, 2000);
+})
+$("#answers5 .answer").click(function(){
+  $("#correct5").css("background-color", "#ee8722");
+  $("#correct5 p").css("color", "black");
+  $("#correct5 span").css("color", "white");
 })
