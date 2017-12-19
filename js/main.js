c8382a4e5d30b9dda934711acda91b4349fb8f44
Jason You <jasonyou92@gmail.com>
2017-12-18 18:17:00 -0800

    responsive images added

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
 //Iteration function of questions 1-4
 $.each([1, 2, 3, 4, 5], function(i) {
-  $("#answers" + i + " .answer").click(function() {
+  $("#correct" + i).click(function() {
     $("#correct" + i).addClass("correct");
+    $("#fighting").addClass("hidden");
+    $("#winning").removeClass("hidden");
     setTimeout(function(){
       $("#question" + i).addClass("hidden");
       $("#answers" + i).addClass("hidden");
       $("#question" + (i + 1)).removeClass("hidden");
       $("#answers" + (i + 1)).removeClass("hidden");
+      $("#fighting").removeClass("hidden");
+      $("#winning").addClass("hidden");
     }, 2000);
-  })
+  });
+  $("#answers" + i + " .answer").click(function() {
+    $("#correct" + i).addClass("correct");
+    $("#fighting").addClass("hidden");
+    $("#losing").removeClass("hidden")
+  });
 });
 //click function for the last question
 $("#answers5 .answer").click(function(){
