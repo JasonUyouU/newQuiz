6dc7b8714915371392de759a0565b4946350b746
Jason You <jasonyou92@gmail.com>
2017-12-20 18:03:07 -0800

    sound effects added

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ $.each([1, 2, 3, 4, 5], function(i) {
     $("#correct" + i).addClass("correct");
     $("#fighting").addClass("hidden");
     $("#winning").removeClass("hidden");
+    $("#sound1").get(0).play();
     setTimeout(function(){
       $("#question" + i).addClass("hidden");
       $("#answers" + i).addClass("hidden");
@@ -18,6 +19,7 @@ $.each([1, 2, 3, 4, 5], function(i) {
     $("#correct" + i).addClass("correct");
     $("#fighting").addClass("hidden");
     $("#losing").removeClass("hidden");
+    $("#sound2").get(0).play();
   });
 });
 //click function for the last question
@@ -26,9 +28,11 @@ $("#correct5").click(function() {
   $("#fighting").addClass("hidden");
   $("#victory").removeClass("hidden");
   $("#prize5").addClass("flash bgcolor");
+  $("#sound3").get(0).play();
 })
 $("#answers5 .answer:not(#correct5)").click(function(){
   $("#correct5").addClass("correct");
   $("#fighting").addClass("hidden");
   $("#losing").removeClass("hidden");
+  $("#sound2").get(0).play();
 });
